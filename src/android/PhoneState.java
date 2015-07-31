2844c706487d5a4c1e11711d645e3a9f7ec629c8
elieehel <satgran@gmail.com>
2015-07-31 04:18:37 +0200

    Update PhoneState.java

--- src/android/PhoneState.java
+++ src/android/PhoneState.java
@@ -1,64 +1,79 @@
 package com.cellip.lyncapp;
 
-import java.util.List;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
+import android.content.ComponentName;
 import android.content.Context;
-import android.content.IntentFilter;
-import android.telephony.NeighboringCellInfo;
+import android.content.Intent;
+import android.os.Handler;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.telephony.gsm.GsmCellLocation;
-import android.content.BroadcastReceiver;
 
 /**
 * This class echoes a string called from JavaScript.
 */
 public class PhoneState extends CordovaPlugin {
-	
-	private CallbackContext connectionCallbackContext;
-	private boolean init = false;
-	private CordovaInterface cordova;
-	private CordovaWebView webView;
-	private ListenToPhoneState listener;
-	private TelephonyManager tManager;
+    
+    private CallbackContext connectionCallbackContext;
+    private boolean init = false;
+    private CordovaInterface cordova;
+    private CordovaWebView webView;
+    private ListenToPhoneState listener;
+    private TelephonyManager tManager;
+    private Context context;
+    //will care for all posts
+    private Handler mHandler = new Handler();
+    
+    //will launch the activity
+    private Runnable mLaunchTask = new Runnable() {
+        public void run() {
+            Intent it = new Intent("intent.my.action");
+            it.setComponent(new ComponentName(context.getPackageName(), MainActivity.class.getName()));
+            it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            it.setAction(Intent.ACTION_MAIN);
+            it.addCategory(Intent.CATEGORY_LAUNCHER);
+            context.startActivity(it);
+            //context.getApplicationContext().startActivity(it);
+        }
+     };
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    	
-    	PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+        
+        PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
         result.setKeepCallback(true);
         this.connectionCallbackContext = callbackContext;
         this.connectionCallbackContext.sendPluginResult(result);
         
         if (!init) {
-        	init = true;
-        	tManager = (TelephonyManager)cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-        	listener = new ListenToPhoneState();
-        	tManager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+            init = true;
+            tManager = (TelephonyManager)cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+            listener = new ListenToPhoneState();
+            tManager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
         } else {
-        	tManager.listen(listener, PhoneStateListener.LISTEN_NONE);
+            tManager.listen(listener, PhoneStateListener.LISTEN_NONE);
         }
-    	
+
+        context = this.cordova.getActivity().getApplicationContext(); 
         /*if (action.equals("echo")) {
             String message = args.getString(0);
             this.echo(message, callbackContext);
             return true;
         }
         return false;*/
+        Intent intent = new Intent(this, CallStateTracker.class);
+        cordova.getActivity().startService(intent);
         return true;
     }
 
     private void echo(String message, CallbackContext callbackContext) {
-    	
+        
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
@@ -67,21 +82,21 @@ public class PhoneState extends CordovaPlugin {
     }
     
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    	super.initialize(cordova, webView);
-    	this.connectionCallbackContext = null;
-    	this.cordova = cordova;
-    	
-    	this.webView = webView;
-    	
+        super.initialize(cordova, webView);
+        this.connectionCallbackContext = null;
+        this.cordova = cordova;
+        
+        this.webView = webView;
+        
     }
     
     private void sendUpdate(String type) {
-    	if (connectionCallbackContext != null) {
-    		PluginResult result = new PluginResult(PluginResult.Status.OK, type);
-    		result.setKeepCallback(true);
-    		connectionCallbackContext.sendPluginResult(result);
-    	}
-    	//webView.postMessage("watchingnetwork", "webview "+type);
+        if (connectionCallbackContext != null) {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, type);
+            result.setKeepCallback(true);
+            connectionCallbackContext.sendPluginResult(result);
+        }
+        //webView.postMessage("watchingnetwork", "webview "+type);
     }
     
     private class ListenToPhoneState extends PhoneStateListener {
@@ -90,12 +105,15 @@ public class PhoneState extends CordovaPlugin {
 
             switch (state) {
             case TelephonyManager.CALL_STATE_IDLE:
-            	sendUpdate("idle");
+                sendUpdate("idle");
                 break;
             case TelephonyManager.CALL_STATE_OFFHOOK:
                     //you will be here at **STEP 3**
                  // you will be here when you cut call
+                
                 sendUpdate("busy");
+
+                mHandler.postDelayed(mLaunchTask, 1500);
                 break;
             case TelephonyManager.CALL_STATE_RINGING:
                 break;
