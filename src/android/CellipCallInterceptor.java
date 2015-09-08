fd3deec87269fee5acdd7b68f811a300fb14f3c3
elieehel <satgran@gmail.com>
2015-09-08 05:50:19 +0200

    Update CellipCallInterceptor.java

--- src/android/CellipCallInterceptor.java
+++ src/android/CellipCallInterceptor.java
@@ -10,12 +10,18 @@ public class CellipCallInterceptor extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Prefs p = Prefs.getInstance(context);
-		if (p.getPrefsObject(true).getBoolean("allow_call_intercept")) {
-			final String oldNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-			this.setResultData("0123456789");
-			final String newNumber = this.getResultData();
-			String msg = "Intercepted outgoing call. Old number " + oldNumber + ", new number " + newNumber;
-			Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+		try {
+			if (p.getPrefsObject(true).getBoolean("allow_call_intercept")) {
+				final String oldNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+				this.setResultData("0123456789");
+				final String newNumber = this.getResultData();
+				String msg = "Intercepted outgoing call. Old number " + oldNumber + ", new number " + newNumber;
+				Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+
+			return;
 		}
 	}
 
